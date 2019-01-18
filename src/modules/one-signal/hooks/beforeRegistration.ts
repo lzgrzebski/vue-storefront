b6bcb912e5be40ab006545d6d58f4fc09c78fcf8
lzgrzebski <lukasz@pexip.com>
2019-01-18 12:19:33 +0100

    wip: update beforeRegistration add check for document and window

--- src/modules/one-signal/hooks/beforeRegistration.ts
+++ src/modules/one-signal/hooks/beforeRegistration.ts
@@ -5,18 +5,21 @@ declare global {
 
 export function beforeRegistration(Vue, config, store, isServer) {
   if (config.oneSignal.id && !isServer) {
-    const oneSignalSDK = document.createElement('script');
-    oneSignalSDK.async = true;
-    oneSignalSDK.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
-    document.head.appendChild(oneSignalSDK);
-
-    window.OneSignal = window.OneSignal || [];
-    window.OneSignal.push(function() {
-      console.log('blabla');
-      window.OneSignal.init({
-        appId: "089787d5-7a25-4efb-9044-b71f75ad96fd",
+    Logger.info('Adding oneSignal extension')()
+    if (typeof document !== 'undefined') {
+      const oneSignalSDK = document.createElement('script');
+      oneSignalSDK.async = true;
+      oneSignalSDK.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
+      document.head.appendChild(oneSignalSDK);
+    }
+    if (typeof window !== 'undefined') {
+      window.OneSignal = window.OneSignal || [];
+      window.OneSignal.push(function() {
+        window.OneSignal.init({
+          appId: "089787d5-7a25-4efb-9044-b71f75ad96fd",
+        });
       });
-    });
+    }
 
   } else {
     Logger.warn('Google Analytics extensions is not working. Ensure Google Analytics account ID is defined in config', 'GA')()
